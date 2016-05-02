59814729c3cb6c336eb7688b20daabcddfa130f2
JuanKman94 <juan.carlos.menonita@gmail.com>
2016-05-02 12:06:15 -0500

    Agrega proyecto MVC

---
+++ web/escuela/escuela/Models/Programa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace escuela.Models
+{
+    public class Programa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Version { get; set; }
+    }
+}
